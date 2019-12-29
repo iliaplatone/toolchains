b9973991c038e4e1bd77f01b6774d4164455ddde
Ilia Platone <info@iliaplatone.com>
2019-12-29 15:49:59 +0100

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
+dir=$(pwd)
 version=$1
 crosstool=$2
 arch=$3
-instdir=$arch/gcc-$version
+instdir=$dir/$arch/gcc-$version
 suffix=$4
 
-packages="binutils-$crosstool cpp-$version-$crosstool gcc-$version-$crosstool-base libstdc++-$version-dev-$arch$suffix libgcc1-$arch$suffix libgcc-$version-dev-$arch$suffix libc6-$arch$suffix libc6-dev-$arch$suffix linux-libc-dev-$arch$suffix gcc-$version-$crosstool g++-$version-$crosstool"
+packages="binutils-$crosstool cpp-$version-$crosstool gcc-$version-$crosstool-base libstdc++-$version-dev-$arch$suffix libgcc1-$arch$suffix libgcc-$version-dev-$arch$suffix libc6-$arch$suffix libc6-dev-$arch$suffix linux-libc-dev-$arch$suffix gcc-$version-$crosstool g++-$version-$crosstool libisl[0-9]* libmpc[0-9]* libmpfr[0-9]*"
 
+mkdir -p $instdir
 mkdir -p temp
 pushd temp
-for deb in $packages; do apt -qqq download $deb; done
-file-roller -h *.deb
-popd
-mkdir -p $instdir
+for deb in $packages; do apt download $deb; done
+for deb in *.deb; do dpkg-deb -x $deb $instdir; done
 pushd $instdir
-for deb in ../../temp/*/; do tar zxf $deb/data.tar.gz 2>/dev/null || tar Jxf $deb/data.tar.xz 2>/dev/null; done
-pushd usr/bin
-for file in *$version; do sudo ln -nfs $file $( echo $file | sed -e "s/-$version//g" ); done
-popd
 rsync -ar usr/ ./
 rm -r usr/
 ln -s ./ usr
 popd
-sudo rm -r temp
+popd
+rm -r temp
